75c4e25e0a361308aa8bf3f10538aa30688dacf9
Matthew Moschella <moschella@princeton.edu>
2021-04-10 11:17:18 -0400

    minimum working example for unit testing

--- tests/test_axis.py
+++ tests/test_axis.py
@@ -1,5 +1,14 @@
 import unittest
 import astropy.units as u
 
+from axionpy import axis
+
 class TestAxis(unittest.TestCase):
-    def test_
+    def test_is_quantity(self):
+        self.assertTrue(axis._is_quantity(1.0*u.s, "time"))
+        self.assertTrue(axis._is_quantity(1.0*u.GeV, "energy"))
+        self.assertFalse(axis._is_quantity(1.0*u.GeV, "time"))
+        self.assertFalse(axis._is_quantity(1.0, "time"))
+
+if __name__=='__main__':
+    unittest.main()
